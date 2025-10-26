84dc2f9701f88a5bacc1007b05fdcb3dd10e1524
Philip Oesterle-Pekrun <philipoesterlepekrun@gmail.com>
2025-10-26 23:24:41 +0100

    Fix CMakeLists.txt for install

--- test/Test1.cpp
+++ test/Test1.cpp
@@ -7,13 +7,13 @@ int Test1(int argCount, char** args) {
   std::string str = "012345";
   int* arr = MyUtils::Strings::checkForIn("1", str, 5);
   
-  std::cout<<"Test1: ";
+  std::cout<<"Test 1.1: ";
   if(arr[0]==1 && arr[1]==-1) std::cout<<"Passed"<<"\n"; 
   else std::cout<<"Failed"<<"\n";
   
   free(arr);
   
-  std::cout<<"Test2: ";
+  std::cout<<"Test 1.2: ";
   if(MyUtils::Math::CommonFunctions::step(-1) == 0) std::cout<<"Passed"<<"\n";
   else std::cout<<"Failed"<<"\n";
   
